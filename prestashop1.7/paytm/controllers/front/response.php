2d9e2d65a0be9f3e3c7ebabecdc850221ae26486
aabhishekgoyal <abhishek.goyal@paytm.com>
2017-08-10 12:57:57 +0530

    Update Order Amount

--- prestashop1.7/paytm/controllers/front/response.php
+++ prestashop1.7/paytm/controllers/front/response.php
@@ -1,5 +1,4 @@
 <?php
-//require_once(dirname(__FILE__).'/../../lib/Rc43.php');
 require_once(dirname(__FILE__).'/../../lib/encdec_paytm.php');
 class PaytmResponseModuleFrontController extends ModuleFrontController {
 	public function postProcess() {
@@ -7,37 +6,18 @@ class PaytmResponseModuleFrontController extends ModuleFrontController {
 		$res_code      = $_POST['RESPCODE'];
 		$res_desc      = $_POST['RESPMSG'];
 		$checksum_recv = $_POST['CHECKSUMHASH'];
-		$paramList     = $_POST;
-		
-		//var_dump($paramList);
+		$paramList     = $_POST;		
 		$secret_key	   = Configuration::get('PayTM_SECRET_KEY');
 		$merchant_id	   = Configuration::get('PayTM_MERCHANT_ID');
 		$order_amount  = $_POST['TXNAMOUNT'];
 				
 		$bool = "FALSE";
-		//echo "<pre>"; print_r($paramList); echo 11;
 		$bool = verifychecksum_e($paramList, $secret_key, $checksum_recv);
 		
-		/*if(isset($DR)){
-			$DR = preg_replace("/\s/","+",$DR);
-			$rc4 = new Crypt_RC4($secret_key);
-			$QueryString = base64_decode($DR);
-			$rc4->decrypt($QueryString);
-			$QueryString = explode('&',$QueryString);
-			$response = array();
-			foreach($QueryString as $param){
-				$param = explode('=',$param);
-				$response[$param[0]] = urldecode($param[1]);
-				array(8) { ["RESPCODE"]=> string(3) "141" ["RESPMSG"]=> string(26) "Cancel Request by Customer" ["STATUS"]=> string(11) "TXN_FAILURE" ["MID"]=> string(20) "pebble49164290093828" ["TXNAMOUNT"]=> string(3) "199" ["ORDERID"]=> string(4) "1105" ["TXNID"]=> string(4) "9051" ["CHECKSUMHASH"]=> string(108) "8JTqSis+Uqe2iVMo/vWLgjFQkay2pZQkoN/uUVaBbkZrwkYEZMXIKfKy9NfYd2Fk9JaHiemzwNVpfRJrqiWzyeDWxZSJBhCi5NBEaTdbcZA=" } 
-			}
-		}*/		
-			
 		$cartID = $order_id;
 		$extras = array();
 		$extras['transaction_id'] = $_POST['TXNID'];
-		//$cart = new Cart(intval($cartID));
 		$cart = $this->context->cart;
-		//echo "<pre>"; print_r($cart); die;
 		$amount = $cart->getOrderTotal(true,Cart::BOTH);
 		$responseMsg1 = $_POST['RESPMSG'];
 		$mode = Configuration::get('PayTM_MODE');
@@ -46,10 +26,8 @@ class PaytmResponseModuleFrontController extends ModuleFrontController {
 			$requestParamList = array("MID" => $merchant_id , "ORDERID" => $_POST['ORDERID']);
 			
 			$StatusCheckSum = getChecksumFromArray($requestParamList,$secret_key);
-					
+				
 			$requestParamList['CHECKSUMHASH'] = $StatusCheckSum;
-			//echo "<pre>"; print_r($requestParamList); die;
-			// Call the PG's getTxnStatus() function for verifying the transaction status.
 			
 			if($mode=="TEST")
 			{
@@ -61,12 +39,11 @@ class PaytmResponseModuleFrontController extends ModuleFrontController {
 			}						
 				if ($res_code == "01") {
 					$responseParamList = callNewAPI($check_status_url, $requestParamList);
-					if($responseParamList['STATUS']=='TXN_SUCCESS' && $responseParamList['TXNAMOUNT']==$order_amount)
+					if($responseParamList['STATUS']=='TXN_SUCCESS' && $responseParamList['TXNAMOUNT']==$amount)
 					{
 						$status_code = "Ok";
 						$message= $responseMsg1;
 						$responseMsg= $responseMsg1;
-				   		// $status = "15" ;
 						$status = Configuration::get('Paytm_ID_ORDER_SUCCESS');
 					}
 					else{
@@ -90,7 +67,6 @@ class PaytmResponseModuleFrontController extends ModuleFrontController {
 			
 			
         } else {
-			//echo "THIS"; die;
 			$status_code = "Failed";
             $responseMsg = "Security Error ..!";
             $message = $responseMsg1;
@@ -108,34 +84,12 @@ class PaytmResponseModuleFrontController extends ModuleFrontController {
 			'this_path' => $this->module->getPathUri(),
 			'this_path_ssl' => Tools::getShopDomainSsl(true, true).__PS_BASE_URI__.'modules/'.$this->module->name.'/'
 		));
-		//$cart_qties == 0;
 		$cart->delete();
-		//$this->setTemplate('payment_response.tpl');
-		//Tools::redirectLink(__PS_BASE_URI__.'order-confirmation.php?key='.$customer->secure_key.'&id_cart='.(int)$this->context->cart->id.'&id_module='.(int)$this->module->id.'&id_order='.(int)$this->module->currentOrder);
-		//$this->setTemplate('module:paytm/views/templates/front/payment_response.tpl');
-		//Tools::redirect('index.php?controller=order-confirmation&id_cart='.(int)$cart->id.'&id_module='.(int)$this->module->id.'&id_order='.$this->module->currentOrder.'&key='.$customer->secure_key);
-		
 		if ($order_id && ($secure_key == $customer->secure_key) && $responseParamList['STATUS']=='TXN_SUCCESS' && $responseParamList['TXNAMOUNT']==$order_amount) {
             $module_id = $this->module->id;
             Tools::redirect('index.php?controller=order-confirmation&id_cart='.(int)$cart->id.'&id_module='.(int)$this->module->id.'&id_order='.$_POST['ORDERID'].'&key='.$customer->secure_key.'&responseMsg='.$message);
         } else {
-            //$this->errors[] = $this->module->l('An error occured. Please contact the merchant to have more informations');
-            //return $this->setTemplate('module:paytm/views/templates/hook/front/error.tpl');
-			//return $this->display(__FILE__, 'views/templates/hook/front/error.tpl');
-			$this->setTemplate('module:paytm/views/templates/hook/payment_response.tpl');
+            $this->setTemplate('module:paytm/views/templates/hook/payment_response.tpl');
         }
-		
-		
-		//Tools::redirect('index.php?controller=order-confirmation&id_cart='.(int)$cart->id.'&id_module='.(int)$this->module->id.'&id_order='.$_POST['ORDERID'].'&key='.$customer->secure_key.'&responseMsg='.$message);
-		
-		
-		//$this->setTemplate('module:paytm/views/templates/hook/payment_response.tpl');
-		
-		
-		/* $url = $this->context->link->getModuleLink('paytm', 'paymentReturn', array(
-            'secure_key' => $this->context->customer->secure_key), true);
-        //PrestaShopLogger::addLog('rediret to payment return : '.$url, 1, null, null, null, true);
-        Tools::redirect($url);
-        exit; */
 }
 }
