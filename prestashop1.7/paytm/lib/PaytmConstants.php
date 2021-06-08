b1fc2fd1c4d5bcfb917f523c904c2edd8791aa08
Abhishek Awasthi <abhishek2.awasthi@paytm.com>
2021-06-08 18:29:59 +0530

    update version

--- prestashop1.7/paytm/lib/PaytmConstants.php
+++ prestashop1.7/paytm/lib/PaytmConstants.php
@@ -22,8 +22,8 @@ class PaytmConstants{
 	 CONST CONNECT_TIMEOUT						= "10";
 	 CONST TIMEOUT								= "10";
 
-	 CONST LAST_UPDATED							= "20210514";
-	 CONST PLUGIN_VERSION						= "2.5";
+	 CONST LAST_UPDATED							= "20210608";
+	 CONST PLUGIN_VERSION						= "3.0";
 	 CONST PLUGIN_DOC_URL						= "https://developer.paytm.com/docs/eCommerce-plugin/prestashop/#v1-7-x";
 
 	 CONST CUSTOM_CALLBACK_URL					= "";
